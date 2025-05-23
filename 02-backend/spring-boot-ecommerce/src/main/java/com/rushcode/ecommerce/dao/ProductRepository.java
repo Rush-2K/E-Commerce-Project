5c66ee4eacc9355580fd69eb2cb384911633ff1b
Rusyaidi Azhari <rusyaidiazhari@Rusyaidis-Laptop.local>
2025-05-23 09:27:17 +0800

    add html table, add product images, add cors policy to backend

--- 02-backend/spring-boot-ecommerce/src/main/java/com/rushcode/ecommerce/dao/ProductRepository.java
+++ 02-backend/spring-boot-ecommerce/src/main/java/com/rushcode/ecommerce/dao/ProductRepository.java
@@ -1,10 +1,12 @@
 package com.rushcode.ecommerce.dao;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
 import com.rushcode.ecommerce.entity.Product;
 
 // specify entity type and primary key type
+@CrossOrigin("http://localhost:4200")
 public interface ProductRepository extends JpaRepository<Product, Long> {
 
 }
