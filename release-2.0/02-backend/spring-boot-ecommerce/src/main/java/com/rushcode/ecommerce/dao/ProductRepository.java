d13a7054d4013af53d14421c420d20e30ea72191
Rusyaidi Azhari <rusyaidiazhari@Rusyaidis-Laptop.local>
2025-05-23 12:11:12 +0800

    enhance product list component to read id param, add new method in REST repo(backend) to find by category id, update angular service to call new URL/endpoint

--- release-2.0/02-backend/spring-boot-ecommerce/src/main/java/com/rushcode/ecommerce/dao/ProductRepository.java
+++ release-2.0/02-backend/spring-boot-ecommerce/src/main/java/com/rushcode/ecommerce/dao/ProductRepository.java
@@ -1,6 +1,9 @@
 package com.rushcode.ecommerce.dao;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.query.Param;
 import org.springframework.web.bind.annotation.CrossOrigin;
 
 import com.rushcode.ecommerce.entity.Product;
@@ -9,4 +12,5 @@ import com.rushcode.ecommerce.entity.Product;
 @CrossOrigin("http://localhost:4200")
 public interface ProductRepository extends JpaRepository<Product, Long> {
 
+    Page<Product> findByCategoryId(@Param("id") Long id, Pageable pageable);
 }
